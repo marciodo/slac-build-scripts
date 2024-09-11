db62841ce733ddebdd51ed083a9ab39469f04d97
Jeremy Lorelli <lorelli@slac.stanford.edu>
2024-09-10 18:53:22 -0700

    Remove old file

--- toolchains/buildroot-2019.08-x86_64.sh
+++
@@ -1,4 +0,0 @@
-
-export TOOLCHAIN="/sdf/sw/epics/package/linuxRT/buildroot-2019.08/host/linux-x86_64/x86_64/"
-export CC="${TOOLCHAIN}/bin/x86_64-buildroot-linux-gnu-gcc"
-export CXX="${TOOLCHAIN}/bin/x86_64-buildroot-linux-gnu-g++"
