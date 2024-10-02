1a2cbab531d150c75d4fd3d6ccd6777ea711f268
Jeremy Lorelli <lorelli@slac.stanford.edu>
2024-10-01 17:57:23 -0700

    export TARGET_SYSTEM and TOOLCHAIN_PATH vars

--- toolchains/buildroot-2019.08-arm.bash
+++ toolchains/buildroot-2019.08-arm.bash
@@ -1,6 +1,6 @@
-TOOLCHAIN_PATH="${EPICS_PACKAGE_TOP}/linuxRT/buildroot-2019.08/host/linux-x86_64/arm/"
+export TOOLCHAIN_PATH="${EPICS_PACKAGE_TOP}/linuxRT/buildroot-2019.08/host/linux-x86_64/arm/"
 
 export TOOLCHAIN_SYSROOT="${TOOLCHAIN_PATH}/arm-buildroot-linux-gnueabi/sysroot"
 export CC="${TOOLCHAIN_PATH}/bin/arm-buildroot-linux-gnueabi-gcc"
 export CXX="${TOOLCHAIN_PATH}/bin/arm-buildroot-linux-gnueabi-g++"
-
+export TARGET_SYSTEM=arm-buildroot-linux-gnueabi
--- toolchains/buildroot-2019.08-i686.bash
+++ toolchains/buildroot-2019.08-i686.bash
@@ -1,6 +1,6 @@
-TOOLCHAIN_PATH="${EPICS_PACKAGE_TOP}/linuxRT/buildroot-2019.08/host/linux-x86_64/i686/"
+export TOOLCHAIN_PATH="${EPICS_PACKAGE_TOP}/linuxRT/buildroot-2019.08/host/linux-x86_64/i686/"
 
 export TOOLCHAIN_SYSROOT="${TOOLCHAIN_PATH}/i686-buildroot-linux-gnu/sysroot"
 export CC="${TOOLCHAIN_PATH}/bin/i686-buildroot-linux-gnu-gcc"
 export CXX="${TOOLCHAIN_PATH}/bin/i686-buildroot-linux-gnu-g++"
-
+export TARGET_SYSTEM=i686-buildroot-linux-gnu
