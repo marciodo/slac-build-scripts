69649b92de697bc9876baef30fc238d45a3d655a
Marcio Donadio <marcio@slac.stanford.edu>
2025-05-15 17:46:56 -0700

    Added Buildroot 2025 to tirpc

--- all-tirpc.sh
+++ all-tirpc.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 VERSIONS="1.3.5"
-CROSS_TARGETS="buildroot-2019.08-x86_64 buildroot-2019.08-i686 buildroot-2019.08-arm"
+CROSS_TARGETS="buildroot-2025.02-x86_64 buildroot-2019.08-x86_64 buildroot-2019.08-i686 buildroot-2019.08-arm"
 
 for ver in $VERSIONS; do
 	./build-tirpc.sh $ver $EPICS_HOST_ARCH
